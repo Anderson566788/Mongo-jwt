5c2ce8fc6777bbfa8f8cb37e9e88ce7733464fea
Anderson Freire <andersongomesfreire566788@gmail.com>
2025-05-20 23:07:18 -0300

    atualizando authController

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-// export async function register(req, reply) {
-//   const { email, password } = req.body;
-//   await User.create({ email, password });
-//   reply.send({ message: 'User created' });
-// }
+export async function register(req, reply) {
+  const { email, password, adminCode } = req.body
+  const role = adminCode === process.env.ADMIN_SECRET ? 'admin' : 'user'
+  await User.create({ email, password, role })
+
+  reply.code(201).send({ message: 'User create', role })
+}
 
 export async function login(req, reply) {
   const { email, password } = req.body;
@@ -15,12 +17,4 @@ export async function login(req, reply) {
   }
   const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
   reply.send({ token });
-}
-
-export async function register(req, reply) {
-  const { email, password, adminCode } = req.body
-  const role = adminCode === process.env.ADMIN_SECRET ? 'admin' : 'user'
-  await User.create({ email, password, role })
-
-  reply.code(201).send({ message: 'User create', role })
 }
